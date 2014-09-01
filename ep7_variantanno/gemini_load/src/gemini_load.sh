a5d24f7a468f2ada9036439ccc3bf8e56dfb8cca
Mike Lin <mlin@mlin.net>
2014-09-01 00:31:57 -0700

    gemini_load built-in VEP

--- ep7_variantanno/gemini_load/src/gemini_load.sh
+++ ep7_variantanno/gemini_load/src/gemini_load.sh
@@ -1,13 +1,46 @@
 #!/bin/bash
 # gemini_load 0.0.1
 
-main() {
-    echo "Value of vcfgz: '$vcfgz'"
-    echo "Value of advanced_options: '$advanced_options'"
+set -e -x -o pipefail
 
-    # TODO: install & run VEP according to
-    # http://gemini.readthedocs.org/en/latest/content/functional_annotation.html#stepwise-installation-and-usage-of-vep
+run_vep() {
+    # install apt dependencies
+    DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-suggests --no-install-recommends libdbi-perl bioperl
 
+    # deploy VEP cache
+    mkdir -p /vep/cache
+    pushd /vep/cache
+    dx cat file-BGYpvVj0p0PYy1FG7GyQ0j6Q | tar x # homo_sapiens_vep_75.tar
+
+    # deploy VEP code
+    cd /vep
+    unzip -q ensembl-tools-75.zip
+    dx download file-BPGyxyj0FVg3GyPPVx9QpfyP -o ensembl-api.tar.gz
+    tar zxf ensembl-api.tar.gz
+    export PERL5LIB=${PERL5LIB}:/vep/ensembl/modules:/vep/ensembl-compara/modules:/vep/ensembl-variation/modules:/vep/ensembl-funcgen/modules
+
+    # download input VCF
+    dx download "$1" -o input.vcf.gz
+
+    # run VEP
+    cd ensembl-tools-release-*/scripts/variant_effect_predictor
+    perl variant_effect_predictor.pl --cache --offline --dir /vep/cache/.vep --species homo_sapiens \
+                                     --sift b \
+                                     --polyphen b \
+                                     --symbol \
+                                     --numbers \
+                                     --biotype \
+                                     --total_length \
+                                     --fields Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE \
+                                     -i /vep/input.vcf.gz --vcf --fork $(nproc)
+    # VEP command-line ref: http://gemini.readthedocs.org/en/latest/content/functional_annotation.html#stepwise-installation-and-usage-of-vep
+
+    # stage annotated VCF
+    cp variant_effect_output.txt /home/dnanexus/my.vcf
+    popd
+}
+
+setup_gemini() {
     wget https://raw.github.com/arq5x/gemini/master/gemini/scripts/gemini_install.py
     mkdir -p /usr/local /usr/local/share/gemini
     python gemini_install.py /usr/local /usr/local/share/gemini
@@ -15,8 +48,25 @@ main() {
 
     gemini update --dataonly --extra cadd_score
     gemini update --dataonly --extra gerp_bp
+}
 
-    dx cat "$vcfgz" | zcat > my.vcf
+main() {
+    echo "Value of vcfgz: '$vcfgz'"
+    echo "Value of vep: '$vep'"
+    echo "Value of advanced_options: '$advanced_options'"
+
+    setup_gemini &
+
+    if [ "$vep" = "true" ]; then
+        run_vep "$vcfgz"
+        advanced_options="-t VEP $advanced_options"
+    else
+        dx cat "$vcfgz" | zcat > /home/dnanexus/my.vcf
+    fi
+
+    wait
+    export PATH=$PATH:/usr/local/gemini/bin
+    cd /home/dnanexus
     gemini load --cores $(nproc) -v my.vcf $advanced_options my.gemini.db
 
     name=`dx describe "$vcfgz" --name`
